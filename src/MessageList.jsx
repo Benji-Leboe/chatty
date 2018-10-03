d9cdaee2fb47d248b4d445476c59967f550c5d4e
Benji Leboe <benjamin.r.leboe@gmail.com>
2018-10-02 20:05:11 -0700

    split notifications and messages

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -2,22 +2,32 @@ import React, { Component } from 'react';
 
 function MessageList(props) {
   let messageElems = props.messages.map(message => {
-    let { id, type, content, username } = message;
-    return type === 'incomingMessage' ? 
-      (
-        <div key={id.toString()} className="message">
-          <span className="message-username">{ username ? username : 'Anonymous' }</span>
-          <span className="message-content">{ content }</span>
-        </div> 
-      ) : ( 
-        <div key={id.toString()} className="notification">
-          <span className="notification-content">{ content }</span>
-        </div>
+    let { id, timestamp, messageContent, username } = message;
+    return (
+      <div key={id.toString()} data-time={timestamp} className="message">
+        <span className="message-username">{ username ? username : 'Anonymous' }</span>
+        <span className="message-content">{ messageContent }</span>
+      </div>
     );
   });
 
+  let notificationElems = props.notifications.map(notification => {
+    let { id, timestamp, notificationContent } = notification;
+    return (
+      <div key={id.toString()} data-time={timestamp} className="notification">
+        <span className="notification-content">{ notificationContent }</span>
+      </div>
+    );
+  });
+
+  let chatList = messageElems.concat(notificationElems);
+  console.log(chatList);
+  chatList.sort((a,b) => {
+    return new Date(a.props["data-time"]) - new Date(b.props["data-time"]);
+  });
+
   return (
-    <div>{messageElems}</div>  
+    <div>{chatList}</div>  
   );
 }
 
